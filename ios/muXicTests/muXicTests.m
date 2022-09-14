5364ed0ff429ad4af68240e271531434674d4d46
notpankaj <iampankaj0409@gmail.com>
2022-09-14 17:36:18 +0530

    app renamed

--- ios/muXicTests/muXicTests.m
+++ ios/muXicTests/muXicTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface nativeModTests : XCTestCase
+@interface muXicTests : XCTestCase
 
 @end
 
-@implementation nativeModTests
+@implementation muXicTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
